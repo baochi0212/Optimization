2808b3dcba45dcd0ad7dfbfef750afade52f12db
baochi0212 <chideptraiak29@gmail.com>
2021-12-22 16:11:04 +0700

    generate input

--- gen_input.py
+++ gen_input.py
@@ -1,23 +1,31 @@
 import numpy as np
 import json
+import argparse
 
-#export input
-n = int(input('Number of N :'))
-k = int(input('Number of k :'))
-d = np.random.randint(1, 4, size=(n + 1))
-t = np.random.randint(1, 4, size=(n + 1, n + 1))
-d[0] = 0
-for i in range(t.shape[0]):
-	for j in range(t.shape[1]):
-		if i == j:
-			t[i, j] = 0
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str)
 
-print(n, k, d)
-print('MATRIX', len(t), len(t[0]))
-dict = {'N': n,
-		'k': k,
-		'd': d.tolist(),
-		't': t.tolist()}
+if __name__ == '__main__':
+	args = parser.parse_args()
+	name = args.name
 
-with open('sample0.json', 'w') as f:
-	json.dump(dict, f, indent=3)
+	#export input
+	n = int(input('Number of N :'))
+	k = int(input('Number of k :'))
+	d = np.random.randint(1, 24, size=(n + 1))
+	t = np.random.randint(1, 24, size=(n + 1, n + 1))
+	d[0] = 0
+	for i in range(t.shape[0]):
+		for j in range(t.shape[1]):
+			if i == j:
+				t[i, j] = 0
+
+	print(n, k, d)
+	print('MATRIX', len(t), len(t[0]))
+	dict = {'N': n,
+			'k': k,
+			'd': d.tolist(),
+			't': t.tolist()}
+
+	with open(name, 'w') as f:
+		json.dump(dict, f, indent=3)
